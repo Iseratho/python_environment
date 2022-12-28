f5eb8aa9fb280c806382cb8f559da2511e73b5a1
Markus Reiter-Haas <iseratho@gmail.com>
2022-12-28 12:50:41 +0100

    Add missing file from last commit

--- generate_ssh_key.sh
+++
@@ -1,1 +0,0 @@
-ssh-keygen -t ed25519 -C "iseratho@gmail.com"
---
+++ setup_git_ssh.sh
@@ -0,0 +1,3 @@
+ssh-keygen -t ed25519 -C "iseratho@gmail.com"
+git config --global user.email "iseratho@gmail.com"
+git config --global user.name "Markus Reiter-Haas"
